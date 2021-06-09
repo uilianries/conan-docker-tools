58dc14104c59b3212dc7f786581dd0137e4e43f4
jgsogo <jgsogo@gmail.com>
2021-06-09 19:17:04 +0200

    use expected for the gcc version

--- modern/_tests/test_compilers/test_gcc.py
+++ modern/_tests/test_compilers/test_gcc.py
@@ -1,11 +1,12 @@
 import pytest
+from fixtures.expected import Expected
 
 
 @pytest.mark.compiler('gcc')
 @pytest.mark.service('builder', 'deploy')
 class TestGccCompiler:
 
-    def test_version(self, container):
+    def test_version(self, container, expected: Expected):
         out, _ = container.exec(['gcc', '--version'])
-        assert out == 'all'
+        assert out == expected.compiler.version
 
